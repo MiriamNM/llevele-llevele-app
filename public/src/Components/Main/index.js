06a0f0a11f462001e10b1c84de77e08b5adf4ea9
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-08-28 09:19:48 -0600

    Added register modal in main button

--- public/src/Components/Main/index.js
+++ public/src/Components/Main/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RegisterModal from '../Modals/RegisterModal';
 import phoneImg from '../../Assets/phoneImg.png';
 
 const Main = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modal, setModal] = useState(null);
+
+  const showModal = (component) => {
+    setModal(component);
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <main className="flex flex-wrap items-center justify-center">
       <div>
@@ -16,10 +32,20 @@ const Main = () => {
         <p className="text-dark font-medium text-3xl text-justify">
           Registrate y organiza de <br />manera profesional  <br />tu inventario.
         </p>
-        <button className="text-red  bg-mint font-normal text-base rounded px-4 py-2 mt-4 hover:bg-light hover:border-light hover:text-red">
+        <button
+          onClick={() => showModal('Register')} 
+          className="text-red  bg-mint font-normal text-base rounded px-4 py-2 mt-4 hover:bg-red hover:border-light hover:text-ligth"
+        >
           Registrate y administra tu inventario
         </button>
       </div>
+      {modal === 'Register' && isModalOpen && (
+          <RegisterModal
+            handleCancel={handleCancel}
+            visible={isModalOpen}
+            onOk={handleOk}
+          />
+        )}
     </main>
   );
 };
